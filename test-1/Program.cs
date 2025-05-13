9a434c9e8196d50de375654c510cd418ea90b03e
shlomo <s0556732899@gmail.com>
2025-05-13 11:52:50 +0300

    commit_yuser

--- test-1/Program.cs
+++ test-1/Program.cs
@@ -9,22 +9,40 @@ namespace test_1
 {
     internal class Program
     {
-        static void examination(int[]nambers)
+        static void examination(List<int> list_nambers)
         {
             int affirmative = 0;
-            foreach(char c in nambers)
+            foreach(int c in list_nambers)
             {
                 if (c > 0)
                 {
                     affirmative += 1;
                 }
             }
-            if (nambers.Length >= 3&& affirmative >= 3)
+            if (affirmative >= 3)
             {
-                menu(nambers);
+                menu(list_nambers);
+            }
+            else
+            {
+                yuser_input();
             }
         }
-        static void menu(int[]nambers)
+        static void yuser_input()
+        {
+            Console.WriteLine("Please enter at least three positive numbers.");
+            List<int> list_nambers = new List<int>();
+            string str_nam=Console.ReadLine();
+            string[] list_str = str_nam.Split();
+            foreach (string c in list_str)
+            {
+                int num = Convert.ToInt32(c);
+                list_nambers.Add(num);
+                examination(list_nambers);
+            }
+        }
+        
+        static void menu(List<int> list_nambers)
         {
             string[] menu_str = {
             "1=Replace the current series",
@@ -48,9 +66,9 @@ namespace test_1
                  valu = Convert.ToInt32(Console.ReadLine());
             }
             while (valu>10|| valu<0);
-            Value_check(valu, nambers);
+            Value_check(valu, list_nambers);
          }
-        static void Value_check(int valu,int[]nambers)
+        static void Value_check(int valu,List<int> list_nambers)
         {
             switch(valu)
             {
@@ -58,28 +76,28 @@ namespace test_1
                     Enter_series();
                     break;
                 case 2:
-                    Show_series(nambers);
+                    Show_series(list_nambers);
                     break;
                 case 3:
-                    Show_series_reverse(nambers);
+                    Show_series_reverse(list_nambers);
                     break;
                 case 4:
-                    Sort_low_to_high(nambers);
+                    Sort_low_to_high(list_nambers);
                     break;
                 case 5:
-                    max(nambers);
+                    max(list_nambers);
                     break;
                 case 6:
-                    min(nambers);
+                    min(list_nambers);
                     break;
                 case 7:
-                    average(nambers);
+                    average(list_nambers);
                     break;
                 case 8:
-                    Long(nambers);
+                    Long(list_nambers);
                     break;
                 case 9:
-                    sum(nambers);
+                    sum(list_nambers);
                     break;
                 case 10:
                     exit();
@@ -88,38 +106,38 @@ namespace test_1
         }
         static void Enter_series()
         {
-
+            yuser_input();
         }
-        static void Show_series(int[] nambers)
+        static void Show_series(List<int> list_nambers)
         {
-            foreach(int c in nambers)
+            foreach(int c in list_nambers)
             {
                 Console.WriteLine(c);
             }
-            menu(nambers);
+            menu(list_nambers);
         }
-        static void Show_series_reverse(int[] nambers) 
+        static void Show_series_reverse(List<int> list_nambers) 
         { 
-           for(int i = nambers.Length-1;i>-1;i--)
+           for(int i = list_nambers.Count-1;i>-1;i--)
             {
-                Console.WriteLine(nambers[i]);
+                Console.WriteLine(list_nambers[i]);
             }
-            menu(nambers);
+            menu(list_nambers);
         }
-        static void Sort_low_to_high(int[] nambers)
+        static void Sort_low_to_high(List<int> list_nambers)
         {
-            int[]copy_nambers=nambers;
-            Array.Sort(copy_nambers);
+            List <int> copy_nambers= list_nambers;
+            copy_nambers.Sort();
             foreach(int c in copy_nambers)
             {
                 Console.WriteLine(c);
             }
-            menu(nambers);
+            menu(list_nambers);
         }
-        static void max(int[] nambers)
+        static void max(List<int> list_nambers)
         {
            int max = 0;
-           foreach(int c in nambers)
+           foreach(int c in list_nambers)
             {
                 if(c > max)
                 {
@@ -128,46 +146,47 @@ namespace test_1
                 
             }
             Console.WriteLine($"the max nam is {max}");
-            menu(nambers);
+            menu(list_nambers);
 
         }
-        static void min(int[] nambers)
+        static void min(List<int> list_nambers)
         {
-            int min = nambers[0];
-            foreach (int c in nambers)
+            int min = list_nambers[0];
+            foreach (int c in list_nambers)
             {
                 if (c < min)
                 {
                     min = c;
                 }
                 Console.WriteLine(min);
-                menu(nambers);
+                menu(list_nambers);
             }
         }
-        static void average(int[] nambers)
+        static void average(List<int> list_nambers)
         {
-                int Length =nambers.Length;
+                int Length = list_nambers.Count;
                 int sum = 0;
-                foreach(int c in nambers)
+                foreach(int c in list_nambers)
             {
                 sum += c;
             }
                 Console.WriteLine(sum/ Length);
-                menu(nambers);
+                menu(list_nambers);
         }
-        static void Long(int[] nambers)
+        static void Long(List<int> list_nambers)
         {
-            Console.WriteLine(nambers.Length);
-            menu(nambers);
+            Console.WriteLine(list_nambers.Count);
+            menu(list_nambers);
         }
-        static void sum(int[] nambers) 
+        static void sum(List<int> list_nambers) 
         {
             int sum = 0;
-            foreach (char c in nambers)
+            foreach (int c in list_nambers)
             {
                 sum += c;
             }
             Console.WriteLine(sum);
+            menu(list_nambers);
         }
         static void exit()
         {
@@ -179,8 +198,7 @@ namespace test_1
 
         static void Main(string[] args)
         {
-            int[] nambers = { 1, 2, 3};
-            examination(nambers);
+            
         }
     }
 }
